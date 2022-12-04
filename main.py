6650d823e0b4eca98d2b748f05ede4961110bcd5
cgkim412 <hermercur@gmail.com>
2022-12-04 22:04:20 +0900

    implement basic gradio app

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ import gradio as gr
 db = FlowerDatabase()
 table = prepare_table("flower_colors.csv")
 symbols = pd.read_csv("all_symbols.csv").word.values.tolist()
+seasons = "spring summer autumn winter".split()
 
 
-def recommend_flowers(img, symbol):
+def recommend_flowers(img, season, symbol):
     ref_colors = extract_colors(img)["rgb"]
     colors = table.batch_query(ref_colors)[:4]
     all_flowers = []
     for name, rgb in colors:
-        flowers = db.get_flowers_by(color=name, symbol=symbol)
-        all_flowers.append(flowers)
+        flowers = db.get_flowers_by(color=name, season=season, symbol=symbol)[:3]
+        all_flowers += flowers
     return all_flowers
 
 
 gr.Interface(
     fn=recommend_flowers,
-    inputs=[gr.Image(type="pil"), gr.Dropdown(symbols)],
+    inputs=[gr.Image(type="pil"), gr.Dropdown(seasons), gr.Dropdown(symbols)],
     outputs="json",
 ).launch(debug=True)
-
